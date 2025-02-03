f324e63d3a653d78e3b3040f50336d87a34331f1
byayoan <byayoan@gmail.com>
2025-02-03 14:55:48 +0700

    add typogrpahy & exception

--- lib/data/services/http_service.dart
+++ lib/data/services/http_service.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class HttpService {
@@ -11,18 +10,5 @@ class HttpService {
   Future<String> getDataFromUrl(String url) async {
     final response = await _client.get(Uri.parse(url));
     return response.body;
-  }
-
-  Future<Uint8List> getByteArrayFromUrl(String url) async {
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        return response.bodyBytes;
-      } else {
-        throw Exception('Failed to load image: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to fetch image');
-    }
   }
 }
